14710f9ef66eb3961f4f4dff1a1db8baf9b8cf5a
Jonathan <jjjooonnn@gmail.com>
2020-06-10 22:36:10 +0200

    Early use case 4 dev
    enter the courier view, there is a button to skip login, then a mock view of the google map as well as a place holder for requests side bar.
    
    changed shopper to courier in various places.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import StartpageView from './views/HomeView';
 import ShopView from './views/ShopView';
 import HelpView from './views/HelpView';
 import AcceptRequestView from './views/AcceptRequestView';
+import CourierView from './views/CourierView';
 import UserLoginView from './views/UserLoginView';
 import UserRegisterView from './views/UserRegisterView';
 
@@ -29,6 +30,7 @@ class App extends React.Component {
                         <Route path="/shop" component={ShopView} />
                         <Route path="/help" component={HelpView} />
                         <Route path="/acceptRequest" component={AcceptRequestView} />
+                        <Route path="/courier" component={CourierView} />
                         <Route path="/login" component={UserLoginView} />
                         <Route path="/register" component={UserRegisterView} />
                     </Switch>
