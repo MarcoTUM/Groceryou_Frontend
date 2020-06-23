0c234b347576422a264a5715858a9a13c599ea35
Jonathan <jjjooonnn@gmail.com>
2020-06-24 01:22:57 +0200

    Presenting username of current login session
    Username displayed next to the logout button.
    
    Next steps:
    
    -css
    -make pull request

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -2,10 +2,15 @@ import React from "react";
 
 import { Link } from 'react-router-dom';
 import UserService from "../services/UserService";
+import store from "../store";
 
 const LogoutButton = () => {
+
+    let name = store.getState().auth.username;
+
     return(
         <div className="LoginRegisterButtonDiv">
+            <h2>Hello, {name}</h2>
             <Link to="/">
                 <button onClick={UserService.logout} className="LoginRegisterButton">Logout</button>
             </Link>
