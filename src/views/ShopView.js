136f05d47737446da063096f2437cbf72c5fba5b
Hahm <hahmmj@gmail.com>
2020-06-26 00:38:10 +0200

    26.06 added shop sections

--- src/views/ShopView.js
+++ src/views/ShopView.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import ShoppingPage from '../components/ShoppingPage';
+import './ShopView.css';
 import { Row, Col, List, Card, Button} from 'antd';
+import { Link } from 'react-router-dom';
+
 class HomeView extends React.Component {
     constructor(props) {
         super(props);
@@ -11,6 +14,9 @@ class HomeView extends React.Component {
 
     }
     
+    shopVegeClicked(){
+        console.log("shop vege clicked");
+    }
       
 
     render(){
@@ -18,7 +24,17 @@ class HomeView extends React.Component {
         const shop_Section = ()=>{
             return(
                 <div>
+                    <div className="Section2" onClick={this.shopVegeClicked}>
+                    <Link to = "/">
+                    <img src="assets/images/ShopFreezer.svg"/>
+                    </Link>
+                    </div>
+                    <div className="Section" onClick={this.shopVegeClicked}>
                     <img src="assets/images/ShopVege2.svg"/>
+                    </div>
+                    <div className="Section3" onClick={this.shopVegeClicked}>
+                    <img src="assets/images/ShopCash.svg"/>
+                    </div>
                 </div>
             );
         }  
@@ -27,12 +43,18 @@ class HomeView extends React.Component {
                 
                 <NavBar />
                 <Row>
-                    <Col span={4}>
+                    <Col span={4} >
+                    <div className="SidePanel">
+                    side panel
+                    </div>
                     </Col>
                     <Col span={16}>
                     {shop_Section()}
                     </Col>
                     <Col span={4}>
+                    <div className="SidePanel2">
+                    side panel 2
+                    </div>
                     </Col>
                 </Row>
             </div>
