47ba11a0235bda14cd4574b2337e8eea8cc520fe
MarcoTUM <ga58len@web.de>
2020-07-12 04:15:10 +0200

    AcceptRequest Button works now.
    The AcceptRequest  Button now adds the courierID of the current user to the request in the database. ALso now the acceptRequest page is only visible for registered couriers.

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,81 +1,106 @@
 import HttpService from "./HttpService";
 import store from "../store";
-import {logout} from "../redux/authActions";
+import { logout } from "../redux/authActions";
 
 export default class UserService {
-    static baserURL() {return "http://localhost:8080/auth"; }
+  static baserURL() {
+    return "http://localhost:8080/auth";
+  }
 
-    static frontEndURL() {return "http://localhost:3000/"}
+  static frontEndURL() {
+    return "http://localhost:3000/";
+  }
 
-    static register(user,pass) {
-        return new Promise((resolve,reject) => {
-            HttpService.post(UserService.baserURL() + '/register', {
-                username: user,
-                password: pass
-            }, (data) => {
-                resolve(data);
-                window.location.assign(UserService.frontEndURL());
-            }, (textStatus) => {
-                reject(textStatus);
-            });
-        });
-    }
+  static register(user, pass) {
+    return new Promise((resolve, reject) => {
+      HttpService.post(
+        UserService.baserURL() + "/register",
+        {
+          username: user,
+          password: pass,
+        },
+        (data) => {
+          resolve(data);
+          window.location.assign(UserService.frontEndURL());
+        },
+        (textStatus) => {
+          reject(textStatus);
+        }
+      );
+    });
+  }
 
-    static login(user,pass) {
-        return new Promise((resolve,reject) => {
-            HttpService.post(UserService.baserURL() + '/login', {
-                username: user,
-                password: pass
-            }, (data) => {
-                resolve(data);
-                window.location.assign(UserService.frontEndURL());
-            }, (textStatus) => {
-                reject(textStatus);
-            });
-        });
-    }
+  static login(user, pass) {
+    return new Promise((resolve, reject) => {
+      HttpService.post(
+        UserService.baserURL() + "/login",
+        {
+          username: user,
+          password: pass,
+        },
+        (data) => {
+          resolve(data);
+          window.location.assign(UserService.frontEndURL());
+        },
+        (textStatus) => {
+          reject(textStatus);
+        }
+      );
+    });
+  }
 
-    static logout(){
-        //window.localStorage.removeItem('jwtToken'); //deprecated -> use the auth reducer
-        store.dispatch(logout());
-        return new Promise((resolve, reject) => {
-            HttpService.get(UserService.baserURL() + '/logout', (data) => {
-                window.location.assign(UserService.frontEndURL());
-                resolve(data)
-            }, (statusText) => {
-                reject(statusText)
-            });
-        });
+  static logout() {
+    //window.localStorage.removeItem('jwtToken'); //deprecated -> use the auth reducer
+    store.dispatch(logout());
+    return new Promise((resolve, reject) => {
+      HttpService.get(
+        UserService.baserURL() + "/logout",
+        (data) => {
+          window.location.assign(UserService.frontEndURL());
+          resolve(data);
+        },
+        (statusText) => {
+          reject(statusText);
+        }
+      );
+    });
+  }
 
-    }
+  static getCurrentUser() {
+    // let token = window.localStorage['jwtToken']; //deprecated
+    let token = store.getState().auth.token;
+    if (!token || token === "null" || token === null) return {};
 
-    static getCurrentUser() {
-        // let token = window.localStorage['jwtToken']; //deprecated
-        let token = store.getState().auth.token;
-        if(!token || token === 'null' || token === null) return {};
+    let base64Url = token.split(".")[1];
+    let base64 = base64Url.replace("-", "+").replace("_", "/");
+    return {
+      id: JSON.parse(window.atob(base64)).id,
+      username: JSON.parse(window.atob(base64)).username,
+    };
+  }
 
-        let base64Url = token.split('.')[1];
-        let base64 = base64Url.replace('-','+').replace('_','/');
-        return {
-            id: JSON.parse(window.atob(base64)).id,
-            username: JSON.parse(window.atob(base64)).username
-        };
-    }
+  static isAuthenticated() {
+    // return window.localStorage['jwtToken']; //deprecated -> use the auth reducer
+    return (
+      store.getState().auth.token !== null &&
+      store.getState().auth.token !== "null"
+    );
+  }
 
-    static isAuthenticated(){
-        // return window.localStorage['jwtToken']; //deprecated -> use the auth reducer
-        return store.getState().auth.token !== null && store.getState().auth.token !== 'null';
-    }
-
-    static isCourier() {
-        return new Promise((resolve, reject) => {
-            HttpService.post(UserService.baserURL() + '/amICourier',{
-                id: UserService.getCurrentUser().id
-            },(data) =>{
-                resolve(data)
-            }, (textStatus) => {
-                reject(textStatus)
-            });
-        });
-    }
+  static isCourier() {
+    return new Promise((resolve, reject) => {
+      HttpService.post(
+        UserService.baserURL() + "/amICourier",
+        {
+          id: UserService.getCurrentUser().id,
+        },
+        (data) => {
+          resolve(data.isCourier);
+        },
+        (textStatus) => {
+          reject(textStatus);
+        }
+      );
+    });
+  }
 }
--- src/views/AcceptRequestView.js
+++ src/views/AcceptRequestView.js
@@ -2,14 +2,17 @@ import React from 'react';
 import styles from "./AcceptRequestView.module.css"
 
 import { connect } from 'react-redux';
-import { fetchCurrentRequest } from '../redux/currentRequestActions';
+import { fetchCurrentRequest, acceptCurrentRequest } from '../redux/currentRequestActions';
 import { fetchAcceptedRequests } from '../redux/acceptedRequestActions';
 import { fetchCustomers } from '../redux/customerActions';
 
 import { IconContext } from "react-icons";
 import { BsFillPersonFill, BsClock } from "react-icons/bs";
 import { MdEuroSymbol, MdShoppingBasket } from "react-icons/md";
+
 import RequestCard from '../components/RequestCard';
+import UserService from "../services/UserService";
+import Geocoder from "../services/GeocoderService";
 
 import { Spin } from "antd";
 
@@ -35,7 +38,21 @@ const mapStateToProps = (state) => {
         let currentRequestData = currentRequest.currentRequestData;
         let acceptedRequestsData = acceptedRequests.acceptedRequestsData;
         let userData = customersList.customersListData["0"].userData;
-        
+
+        let currentRequestCourierId;
+        if(currentRequestData.hasOwnProperty("courierID")) {
+            currentRequestCourierId = currentRequestData.courierID;
+        } else {
+            currentRequestCourierId = undefined;
+        }
+        /*
+        for(let request in acceptedRequestsData) {
+            if(request.courierID === this.state.currentUserID) {
+
+            }
+        }
+        */
+
         return {
             customerFullName: [userData.name, userData.surname].join(" "),
             customerSurname: userData.surname,
@@ -44,13 +61,15 @@ const mapStateToProps = (state) => {
             PLZ: userData.address.PLZ,
             city: userData.address.city,
             houseNr: userData.address.houseNr,
+            currentRequestId: currentRequestData._id,
             commission: currentRequestData.commission,
             amountOfItems: currentRequestData.itemList.length,
+            currentRequestCourierId: currentRequestCourierId,
             desiredDeliveryTimeStart: currentRequestData.desiredDeliveryTimeStart,
             desiredDeliveryTimeEnd: currentRequestData.desiredDeliveryTimeEnd,
-            acceptedRequest0: {id: acceptedRequestsData["0"]["_id"], userID: acceptedRequestsData["0"]["userID"]},
-            acceptedRequest1: {id: acceptedRequestsData["1"]["_id"], userID: acceptedRequestsData["1"]["userID"]},
-            acceptedRequest2: {id: acceptedRequestsData["2"]["_id"], userID: acceptedRequestsData["2"]["userID"]}
+            acceptedRequest0: { id: acceptedRequestsData["0"]["_id"], userID: acceptedRequestsData["0"]["userID"] },
+            acceptedRequest1: { id: acceptedRequestsData["1"]["_id"], userID: acceptedRequestsData["1"]["userID"] },
+            acceptedRequest2: { id: acceptedRequestsData["2"]["_id"], userID: acceptedRequestsData["2"]["userID"] }
         }
     }
 }
@@ -59,60 +78,133 @@ const mapDispatchToProps = (dispatch) => {
     return {
         fetchCurrentRequest: () => { dispatch(fetchCurrentRequest()) },
         fetchAcceptedRequests: () => { dispatch(fetchAcceptedRequests()) },
-        fetchCustomers: () => { dispatch(fetchCustomers()) }
+        fetchCustomers: () => { dispatch(fetchCustomers()) },
+        acceptCurrentRequest: (RequestID, courierID) => {dispatch(acceptCurrentRequest(RequestID, courierID))}
     }
 }
 
 class AcceptRequestView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAuthenticated: false,
+            currentUserID: 0,
+            isCourier: false
+        };
+    }
+
     componentDidMount() {
+        this.setState({
+            isAuthenticated: UserService.isAuthenticated(),
+            currentUserID: UserService.getCurrentUser().id
+        });
+        UserService.isCourier()
+        .then((response) => {
+            this.setState({
+                isCourier: response
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
         this.props.fetchCurrentRequest();
         this.props.fetchAcceptedRequests();
         this.props.fetchCustomers();
+        
+        let latLong = {};
+        Geocoder(3, "Boltzmannstraße", "Garching", 85748, "Deutschland")
+        .then((response) => {
+            latLong = response;
+            console.log(latLong);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    acceptCurrentRequest = () => {
+        this.props.acceptCurrentRequest(this.props.currentRequestId, this.state.currentUserID);
     }
 
     render() {
-        // If the page is still loading display a loading spinner
-        if (this.props.loading)
-            return <Spin />;
-        return (
-            <main>
-                <div className={styles.row}>
-                    <div className={[styles.column, styles.left].join(" ")}>
-                        <RequestCard customer={this.props.acceptedRequest0.userID} />
-                        <RequestCard customer={this.props.acceptedRequest1.userID} />
-                        <RequestCard customer={this.props.acceptedRequest2.userID} />
-                    </div>
-                    <div className={[styles.column, styles.middle].join(" ")}>
-                        <Map center={[48.262473, 11.668891]} zoom={13}>
-                            <TileLayer
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                            />
-                            <Marker position={[48.262473, 11.668891]} icon={redMarker}>
-                                <Popup>This is the TUM.<br />Best Uni ever!</Popup>
-                            </Marker>
-                        </Map>
-                    </div>
-                    <div className={[styles.column, styles.right].join(" ")}>
-                        <IconContext.Provider value={{ size: "1.5em", verticalAlign: 'middle' }}>
-                            <h3 className={styles.yellowText}><BsFillPersonFill /> {this.props.customerFullName}</h3>
-                            {this.props.street}
-                            <h3 className={styles.yellowText}><MdEuroSymbol /> Commission </h3>
-                            {this.props.commission}€
-                            <h3 className={styles.yellowText}><MdShoppingBasket /> Amount of items </h3>
-                            {this.props.amountOfItems} items
-                            <h3 className={styles.yellowText}><BsClock /> Desired delivery time </h3>
-                            {new Date(this.props.desiredDeliveryTimeStart).toLocaleDateString("en-US")} &nbsp;
-                            {new Date(this.props.desiredDeliveryTimeStart).toLocaleTimeString("en-US")} <br />
-                            &#126; <br />
-                            {new Date(this.props.desiredDeliveryTimeEnd).toLocaleDateString("en-US")} &nbsp;
-                            {new Date(this.props.desiredDeliveryTimeEnd).toLocaleTimeString("en-US")}
-                        </IconContext.Provider>
-                        <button className={styles.acceptButton}>Accept request</button>
-                    </div>
-                </div>
-            </main>
-        );
+        if (this.state.isAuthenticated && this.state.isCourier) {
+            // If the page is still loading display a loading spinner
+            if (this.props.loading)
+                return <Spin />;
+            else {
+                let acceptRequestButton;
+                console.log(this.props.currentRequestCourierId);
+                if(this.props.currentRequestCourierId === undefined) {
+                    acceptRequestButton =
+                    <button 
+                        className={styles.acceptButton} 
+                        onClick={this.acceptCurrentRequest}>
+                        Accept request
+                    </button>;
+                } else {
+                    acceptRequestButton =
+                    <div>
+                        <br/>
+                        <br/>
+                        <p className={styles.alreadyAcceptedRequest}>Already accepted!</p>
+                    </div>;
+                }
+                return (
+                    <main>
+                        <div className={styles.row}>
+                            <div className={[styles.column, styles.left].join(" ")}>
+                                <RequestCard customer={this.props.acceptedRequest0.userID} />
+                                <RequestCard customer={this.props.acceptedRequest1.userID} />
+                                <RequestCard customer={this.props.acceptedRequest2.userID} />
+                            </div>
+                            <div className={[styles.column, styles.middle].join(" ")}>
+                                <Map center={[48.262473, 11.668891]} zoom={13}>
+                                    <TileLayer
+                                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                                    />
+                                    <Marker position={[48.262473, 11.668891]} icon={redMarker}>
+                                        <Popup>This is the TUM.<br />Best Uni ever!</Popup>
+                                    </Marker>
+                                </Map>
+                            </div>
+                            <div className={[styles.column, styles.right].join(" ")}>
+                                <IconContext.Provider value={{ size: "1.5em", verticalAlign: 'middle' }}>
+                                    <h3 className={styles.yellowText}><BsFillPersonFill /> {this.props.customerFullName}</h3>
+                                    {this.props.street}
+                                    <h3 className={styles.yellowText}><MdEuroSymbol /> Commission </h3>
+                                    {this.props.commission}€
+                                <h3 className={styles.yellowText}><MdShoppingBasket /> Amount of items </h3>
+                                    {this.props.amountOfItems} items
+                                <h3 className={styles.yellowText}><BsClock /> Desired delivery time </h3>
+                                    {new Date(this.props.desiredDeliveryTimeStart).toLocaleDateString("en-US")} &nbsp;
+                                {new Date(this.props.desiredDeliveryTimeStart).toLocaleTimeString("en-US")} <br />
+                                &#126; <br />
+                                    {new Date(this.props.desiredDeliveryTimeEnd).toLocaleDateString("en-US")} &nbsp;
+                                {new Date(this.props.desiredDeliveryTimeEnd).toLocaleTimeString("en-US")}
+                                </IconContext.Provider>
+                                {acceptRequestButton}
+                            </div>
+                        </div>
+                    </main>
+                );
+            }
+        }
+        else if (this.state.isAuthenticated && !this.state.isCourier) {
+            return (
+                <main>
+                    Only couriers are able to accept requests.
+                </main>
+            );
+        }
+        else {
+            return (
+                <main>
+                    You need to login to view this page.
+                </main>
+            );
+        }
     }
 }
 
