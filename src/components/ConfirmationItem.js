bdabfe123d35b09abc2ce71e6ba265334dfc83f0
Jonathan <jjjooonnn@gmail.com>
2020-07-14 10:57:11 +0200

    Visual change to items
    Based on their confirmation status, buttons of items are highlited

--- src/components/ConfirmationItem.js
+++ src/components/ConfirmationItem.js
@@ -4,6 +4,7 @@ import {Modal} from "antd";
 import styles from "./ConfirmationItem.module.css"
 import store from "../store";
 import {conf_confirm, conf_replace} from "../redux/confirmationActions";
+import {confirm_state} from "../shared/confirmStates";
 
 function ConfirmationItem(props){
 
@@ -16,21 +17,29 @@ function ConfirmationItem(props){
         store.dispatch(conf_replace(props.id));
     };
 
+    let vButton = <button
+        className={styles.vButton}
+        onClick={confirm}
+    >V</button>;
+
+    if(props.state === confirm_state.confirm){
+        vButton = <button
+            className={styles.vButtonPressed}
+            onClick={confirm}
+        >V</button>;
+    }
+
     return(
         <div className={styles.content}>
             <h2>{props.content.name}</h2>
             <h2>{props.content.amount}{props.content.unitType}</h2>
             <h2>{(props.content.unitPrice * props.content.amount)}</h2>
             <div>
-                <button
-                    className={styles.vButton}
-                    onClick={confirm}
-                >V</button>;
+                {vButton}
                 <button
                     className={styles.xButton}
                     onClick={replace}
                 >X</button>
-                <Modal></Modal>
             </div>
         </div>
     )
