75587b42b5c113729ef65a3ffe55e2c493b8df15
Jonathan <jjjooonnn@gmail.com>
2020-07-14 23:42:32 +0200

    Added CSSs to confirmation
    List has basic CSS to reflect state of replacement offers

--- src/components/ReplacementItem.js
+++ src/components/ReplacementItem.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./ReplacementItem.module.css"
 
 function ReplacementItem(props){
 
     return(
-        <div>
+        <div className={styles.content}>
             <h2>{props.content.name}</h2>
             <h2>{props.content.qty}</h2>
             <h2>{props.content.price}</h2>
--- src/components/ReplacementList.js
+++ src/components/ReplacementList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReplacementItem from "./ReplacementItem";
+import styles from "./ReplacementList.module.css"
 
 class ReplacementList extends React.Component{
 
@@ -13,7 +14,6 @@ class ReplacementList extends React.Component{
         let key = 0;
         for(let prop in this.props.replacements){
             if (Object.prototype.hasOwnProperty.call(this.props.replacements,prop)){
-                console.log(this.props.replacements[prop]);
                 components.push(
                     <ReplacementItem
                         content = {this.props.replacements[prop]}
@@ -25,6 +25,11 @@ class ReplacementList extends React.Component{
 
         return(
             <div>
+                <div className={styles.content}>
+                    <h2>Product</h2>
+                    <h2>Amount</h2>
+                    <h2>Price €</h2>
+                </div>
                 {components}
             </div>
         );
