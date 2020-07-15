09daeeae1ff266dd32634291b4175cb14d81e5b9
Jonathan <jjjooonnn@gmail.com>
2020-07-15 23:50:59 +0200

    Sms functionality
    Now has a complete report of missing items as well as replacements for those.
    
    Still raises an exception AFTER sending out SMS correctly, will investigate

--- src/services/SmsService.js
+++ src/services/SmsService.js
@@ -1,34 +1,49 @@
 import HttpService from "./HttpService";
 import {serverUrl} from "../shared/serverUrl"
 import store from "../store";
+import {confirm_state} from "../shared/confirmStates";
 
 export default class SmsService {
     static baseURL() {return serverUrl + "/sms";}
 
-    static sendChangeRequest(content){
-        return new Promise((resolve,reject) => {
-            let message = "Dear Customer,\n" +
-            "Unfortunately, not all listed items have been found in the requested store. Please have a look at the " +
-            "suggested replacements made by your courier:\n\n";
-
-            message += "DEMO CONTENT\n\n";
-
-            message += "Sincerely, the Groceryou team.";
-
-            HttpService.post(SmsService.baseURL(), {
-                body: message,
-                number: content.number
-            }, (data) => {
-                resolve(data);
-            }, (textStatus) => {
-                reject(textStatus);
-            });
-        });
-    }
-
     static sendConfirmation(content){
         return new Promise((resolve,reject) => {
             let message = "Dear customer, \n Your items have been purchased and are on their way!";
+
+            //find if there are missing objects
+            let missing = [];
+
+            for(let item in content.replacements.items){
+                if(content.replacements.items[item].state != confirm_state.confirm)
+                    missing.push(content.replacements.items[item].content.name);
+            }
+
+            if(missing.length !== 0){
+                message += "\n\nThe following items were unavailable at the specified store:\n";
+                for (let item in missing)
+                    message += missing[item] +"\n"
+            }
+
+            message+="\n";
+
+            let replacements = [];
+
+            for(let prop in content.replacements.replacement){
+                if(Object.prototype.hasOwnProperty.call(content.replacements.replacement, prop)){
+                    const price =  content.replacements.items[prop].content.unitPrice *
+                        content.replacements.items[prop].content.amount;
+                    replacements.push(
+                        content.replacements.items[prop].content.name +
+                        " for " + price + "€" +
+                        " Has been replaced with: " +
+                        content.replacements.replacement[prop].name + " for  " +
+                        content.replacements.replacement[prop].price +"€\n"
+                    );
+                }
+            }
+
+            for(let replacement in replacements)
+                message+= replacements[replacement];
 
             HttpService.post(SmsService.baseURL(), {
                 body: message,
