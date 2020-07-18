5f9c40cc994e2c961230d71f3f08db382fac324e
MarcoTUM <ga58len@web.de>
2020-07-18 02:41:13 +0200

    Basic Modal works and it is no longer possible to accept more than 3 requests.

--- src/components/RequestCard.js
+++ src/components/RequestCard.js
@@ -6,34 +6,71 @@ import styles from "./RequestCard.module.css";
 import { connect } from "react-redux";
 import { fetchCurrentRequest } from "../redux/currentRequestActions";
 
+import { Modal } from "antd";
+import RequestInformation from '../components/RequestInformation';
+
 class RequestCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      modalVisible: false
+    };
+    this.handleConfirmationClick = this.handleConfirmationClick.bind(this);
   }
 
-  buttonOnClick(requestID) {
+  handleConfirmationClick(requestID) {
     this.props.fetchCurrentRequest(requestID);
-  }
+  };
 
   render() {
     return (
       <div className={styles.requestCard}>
         <h3 className={styles.requestCardText}>{this.props.customer}</h3>
         <div className={styles.requestCardButtonContainer}>
-          <button className={styles.requestCardButton}>View details</button>
+          <button
+            className={styles.requestCardButton}
+            onClick={this.showModal}
+          >
+            View details
+          </button>
         </div>
         <Link to="confirmation" className={styles.requestCardButtonContainer}>
           <button
             className={styles.requestCardButton}
-            onClick={() => this.buttonOnClick(this.props.requestID)}
+            onClick={() => this.handleConfirmationClick(this.props.requestID)}
           >
             Confirmation
           </button>
         </Link>
+        <Modal
+            title="Request Detail Information"
+            visible={this.state.modalVisible}
+            onOk={this.modalHandleOk}
+            onCancel={this.modalHandleCancel}
+            >
+            <RequestInformation requestID={this.props.requestID} />
+        </Modal>
       </div>
     );
   }
+
+  modalHandleOk = () => {
+      this.setState({
+          modalVisible: false,
+      });
+  }
+
+  modalHandleCancel = () => {
+      this.setState({
+          modalVisible: false,
+      });
+  }
+
+  showModal = () => {
+      this.setState({
+          modalVisible: true,
+      });
+  };
 }
 
 const mapDispatchToProps = (dispatch) => {
